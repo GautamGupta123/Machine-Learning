c459b647cc7b44c09faf357ff2d92d5bad94a47f
GautamGupta123 <gautamgupta100078@gmail.com>
2023-09-05 18:55:50 +0530

    Artificial Neural Network done

---
+++ ArtifiacialNeuralNetwork.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+#Loading Dataset
+data = pd.read_csv("Churn_Modelling.csv")
+X = data.iloc[:,3:-1].values
+print(X)
+
+from sklearn.preprocessing import LabelEncoder
+LE1 = LabelEncoder()
+X[:,2] = np.array(LE1.fit_transform(X[:,2]))
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct =ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[1])],remainder="passthrough")
+X = np.array(ct.fit_transform(X))
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+ann = tf.keras.models.Sequential()
+ann.add(tf.keras.layers.Dense(units=6,activation="relu"))
+ann.add(tf.keras.layers.Dense(units=1,activation="sigmoid"))
+ann.compile(optimizer="adam",loss="binary_crossentropy",metrics=['accuracy'])
