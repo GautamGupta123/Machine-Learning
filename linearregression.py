2c2c2ea419c10ca3fa616abe5ab5c23c488888c7
GautamGupta123 <gautamgupta100078@gmail.com>
2023-09-05 17:07:13 +0530

    Linear Regression

---
+++ linearregression.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_predict
+house=pd.read_csv("USA_Housing.csv")
+house.head()
+print(house.shape)
+house.describe()
+print(house.columns)
+X=house[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
+           'Avg. Area Number of Bedrooms', 'Area Population', 'Price']]
+y=house['Price']
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+lm=LinearRegression()
+print(X_train)
+lm.fit(X_train,y_train)
+coeff=pd.DataFrame(lm.coef_,X.columns,columns=['Coefficent'])
+print(coeff)
