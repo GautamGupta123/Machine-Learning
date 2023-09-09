73f1a497eb4dffe799fc95111629194fb1429e82
GautamGupta123 <gautamgupta100078@gmail.com>
2023-09-09 16:01:50 +0530

    ANN implementation

--- ArtifiacialNeuralNetwork.py
+++ ArtifiacialNeuralNetwork.py
@@ -4,15 +4,23 @@ import numpy as np
 data = pd.read_csv("csv/Churn_Modelling.csv")
 X = data.iloc[:,3:-1].values
 print(X)
+
+Y = data.iloc[:,-1].values
+
 import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
+
 LE1 = LabelEncoder()
 X[:,2] = np.array(LE1.fit_transform(X[:,2]))
+
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+
 ct =ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[1])],remainder="passthrough")
 X = np.array(ct.fit_transform(X))
+
 from sklearn.model_selection import train_test_split
+
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
