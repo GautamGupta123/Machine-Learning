b73793b4f2dce0e4f56b9af19422c1bae1e4bc49
GautamGupta123 <gautamgupta100078@gmail.com>
2023-09-27 01:07:32 +0530

    logistic regression

--- logisticregression.py
+++ logisticregression.py
@@ -1,33 +1,22 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-house=pd.read_csv("csv/USA_housing.csv")
-house.head(5)
-house.drop(['Address'],axis=1)
-house.shape
-house.describe()
-x=house.iloc[:,[2,3]].values
-y=house.iloc[:,4].values
-print(x)
-house['Address']=house['Address'].astype('category')
-house['Address']=house['Address'].cat.codes
-print(y)
+
+data=pd.read_csv("csv/heart.csv")
+X=data.iloc[:5,0:2]
+print(X)
+print(data.head(5))
+
+plt.scatter(data.age,data.sex,marker='+',color='red')
+plt.show()
+
 from sklearn.model_selection import train_test_split
-xtrain,xtest,ytrain,ytest=train_test_split(x,y,test_size=0.25,random_state=0)
-from sklearn.preprocessing import StandardScaler
-sc=StandardScaler()
-xtrain=sc.fit_transform(xtrain)
-xtest=sc.fit_transform(xtest)
-print(xtrain[0:10,:])
+xtrain, xtest ,ytrain, ytest =  train_test_split (data.age,data.sex,test_size=0.3)
+
+print(xtest)
+
 from sklearn.linear_model import LogisticRegression
-  
-c = LogisticRegression(random_state = 0)
-c.fit(xtest,ytrain)
-pred = c.predict(xtest)
-from sklearn.metrics import confusion_matrix
-  
-confusem = confusion_matrix(ytest, pred)
-print ("Confusion Matrix : \n", confusem)
-from sklearn.metrics import accuracy_score
-  
-print ("Accuracy : ", accuracy_score(ytest, pred))
+model=LogisticRegression()
+model.fit(xtrain,ytrain)
+model.predict(xtest,ytrain)
+model.score(xtest,ytest)
